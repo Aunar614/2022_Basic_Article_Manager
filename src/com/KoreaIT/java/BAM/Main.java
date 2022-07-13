507beb026b89cb8e7e391b11f2d11beb061820b3
Aunar614 <developkim614@gmail.com>
2022-07-13 18:28:54 +0900

    게시물 작성 기능 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -1,22 +1,47 @@
 package com.KoreaIT.java.BAM;
+
 import java.util.Scanner;
 
 public class Main {
 	public static void main(String[] args) {
 		System.out.println("==프로그램 시작==");
-		
+
 		Scanner sc = new Scanner(System.in);
-		
+
+		int articleNum = 0;
+
 		while (true) {
-			
+
 			System.out.printf("명령어 )");
 			String cmd = sc.nextLine();
-			
-			if(cmd.equals("exit")) {
+
+			if (cmd.length() == 0) {
+				System.out.println("명령어를 입력해주세요");
+				continue;
+			}
+
+			if (cmd.equals("article list")) {
+				
+				System.out.println("게시글이 없습니다.");
+				
+			} else if (cmd.equals("article write")) {
+				int id = articleNum + 1;
+				System.out.printf("제목 : ");
+				String title = sc.nextLine();
+				System.out.printf("내용 : ");
+				String body = sc.nextLine();
+				articleNum ++;
+				System.out.printf("%d 번 글이 생성되었습니다.\n", id);
+				
+			} else {
+				System.out.println("존재하지 않는 명령어 입니다.");
+			}
+
+			if (cmd.equals("exit")) {
 				break;
 			}
 		}
-		
+
 		System.out.println("==프로그램 끝==");
 		sc.close();
 	}
