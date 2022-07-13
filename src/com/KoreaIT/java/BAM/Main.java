7a5c493458091a051d49885a39299caf0934a5fd
Aunar614 <developkim614@gmail.com>
2022-07-13 18:52:14 +0900

    게시물 리스트 기능 구현중

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -13,7 +13,7 @@ public class Main {
 		while (true) {
 
 			System.out.printf("명령어 )");
-			String cmd = sc.nextLine();
+			String cmd = sc.nextLine().trim();
 
 			if (cmd.length() == 0) {
 				System.out.println("명령어를 입력해주세요");
@@ -21,18 +21,20 @@ public class Main {
 			}
 
 			if (cmd.equals("article list")) {
-				
+
 				System.out.println("게시글이 없습니다.");
-				
+
 			} else if (cmd.equals("article write")) {
 				int id = articleNum + 1;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-				articleNum ++;
-				System.out.printf("%d 번 글이 생성되었습니다.\n", id);
 				
+				Article article = new Article(id, title, body);
+
+				System.out.printf("%d 번 글이 생성되었습니다.\n", id);
+
 			} else {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
@@ -44,5 +46,17 @@ public class Main {
 
 		System.out.println("==프로그램 끝==");
 		sc.close();
+	}
+}
+
+class Article{
+	int id;
+	String title;
+	String body;
+	
+	Article(int id, String title, String body){
+		this.id = id;
+		this.title = title;
+		this.body = body;
 	}
 }
