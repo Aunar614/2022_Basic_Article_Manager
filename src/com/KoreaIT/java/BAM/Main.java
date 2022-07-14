d7b76ba3a39e60888569b19d8cb9083c5f7ef73c
Aunar614 <developkim614@gmail.com>
2022-07-14 16:38:42 +0900

    게시물 구현

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -11,7 +11,7 @@ public class Main {
 		Scanner sc = new Scanner(System.in);
 
 		int articleNum = 0;
-		
+
 		List<Article> articles = new ArrayList<>();
 
 		while (true) {
@@ -23,19 +23,32 @@ public class Main {
 				System.out.println("명령어를 입력해주세요");
 				continue;
 			}
+			
+			if (cmd.equals("exit")) {
+				break;
+			}
 
 			if (cmd.equals("article list")) {
-
-				System.out.println("게시글이 없습니다.");
+				if (articles.size() == 0) {
+					System.out.println("게시물이 없습니다.");
+					continue;
+				}
+				System.out.println("번호   :   제목");
+				for (int i = articles.size()-1; i>=0; i--) {
+					Article article = articles.get(i);
+					
+					System.out.printf("%d   :   %s\n", article.id, article.title);
+				}
 
 			} else if (cmd.equals("article write")) {
 				int id = articleNum + 1;
+				articleNum = id;
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
 
-				Article article = new Article(id, title, body);
+				articles.add(new Article(id, title, body));
 
 				System.out.printf("%d 번 글이 생성되었습니다.\n", id);
 
@@ -43,9 +56,6 @@ public class Main {
 				System.out.println("존재하지 않는 명령어 입니다.");
 			}
 
-			if (cmd.equals("exit")) {
-				break;
-			}
 		}
 
 		System.out.println("==프로그램 끝==");
