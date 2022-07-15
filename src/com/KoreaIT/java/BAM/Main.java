c75f2158d39bd4e98699dd5a1cb902c31ce583e5
Aunar614 <developkim614@gmail.com>
2022-07-15 18:40:09 +0900

    게시물 작성시 날짜 및 시간 저장, list, detail 날짜 정보 추가, list 줄 정렬

--- src/com/KoreaIT/java/BAM/Main.java
+++ src/com/KoreaIT/java/BAM/Main.java
@@ -33,22 +33,24 @@ public class Main {
 					System.out.println("게시물이 없습니다.");
 					continue;
 				}
-				System.out.println("번호   :    제목");
+				System.out.println("번호   :    제목     :     날짜");
 				for (int i = articles.size() - 1; i >= 0; i--) {
 					Article article = articles.get(i);
 
-					System.out.printf(" %d    :    %s\n", article.id, article.title);
+					System.out.printf(" %d    :    %s     :  %s\n", article.id, article.title, article.regDate);
 				}
 
 			} else if (cmd.equals("article write")) {
 				int id = articleNum + 1;
 				articleNum = id;
+				String regDate = Util.getNowdateStr();
 				System.out.printf("제목 : ");
 				String title = sc.nextLine();
 				System.out.printf("내용 : ");
 				String body = sc.nextLine();
-
-				articles.add(new Article(id, title, body));
+				
+				Article article = new Article(id, regDate, title, body);
+				articles.add(article);
 
 				System.out.printf("%d 번 글이 생성되었습니다.\n", id);
 
@@ -73,7 +75,7 @@ public class Main {
 					continue;
 				} else {
 					System.out.printf("번호 : %d\n", foundArticle.id);
-					System.out.printf("날짜 : 2022-12-12 12:12:12\n");
+					System.out.printf("날짜 : %s\n", foundArticle.regDate);
 					System.out.printf("제목 : %s\n", foundArticle.title);
 					System.out.printf("번호 : %s\n", foundArticle.body);
 				}
@@ -82,9 +84,9 @@ public class Main {
 
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
-				
+
 				int foundIndex = -1;
-				
+
 				for (int i = 0; i < articles.size(); i++) {
 					Article article = articles.get(i);
 
@@ -93,13 +95,12 @@ public class Main {
 						break;
 					}
 				}
-				
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
 					continue;
 				}
-				
+
 				articles.remove(foundIndex);
 				System.out.printf("%d번 게시물을 삭제했습니다\n", id);
 
@@ -116,12 +117,15 @@ public class Main {
 
 class Article {
 	int id;
+	String regDate;
 	String title;
 	String body;
 
-	Article(int id, String title, String body) {
+	Article(int id, String regDate, String title, String body) {
 		this.id = id;
+		this.regDate = regDate;
 		this.title = title;
 		this.body = body;
+		
 	}
 }
