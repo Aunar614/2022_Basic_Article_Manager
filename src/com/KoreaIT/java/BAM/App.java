dcee92a815f7d53e3edeea459c8e72d55c5c89c6
Aunar614 <developkim614@gmail.com>
2022-07-19 16:42:48 +0900

    ArrayList 순회하는 로직 중복 제거

--- src/com/KoreaIT/java/BAM/App.java
+++ src/com/KoreaIT/java/BAM/App.java
@@ -68,16 +68,7 @@ public class App {
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
 
-				Article foundArticle = null;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
+				Article foundArticle = getArticleById(id);
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
@@ -97,16 +88,7 @@ public class App {
 				String[] cmdBits = cmd.split(" ");
 				int id = Integer.parseInt(cmdBits[2]);
 
-				int foundIndex = -1;
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundIndex = i;
-						break;
-					}
-				}
+				int foundIndex = getArticleIndexById(id);
 
 				if (foundIndex == -1) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다.\n", id);
@@ -122,15 +104,6 @@ public class App {
 				int id = Integer.parseInt(cmdBits[2]);
 
 				Article foundArticle = getArticleById(id);
-
-				for (int i = 0; i < articles.size(); i++) {
-					Article article = articles.get(i);
-
-					if (article.id == id) {
-						foundArticle = article;
-						break;
-					}
-				}
 
 				if (foundArticle == null) {
 					System.out.printf("%d번 게시물은 존재하지 않습니다\n", id);
@@ -156,17 +129,43 @@ public class App {
 		sc.close();
 	}
 
-	private Article getArticleById(int id) {
-		
-		for (int i = 0; i < articles.size(); i++) {
-			Article article = articles.get(i);
+	private int getArticleIndexById(int id) {
+		int i = 0;
+		for (Article article : articles) {
 
 			if (article.id == id) {
-				foundArticle = article;
-				break;
+				return i;
 			}
+			i++;
+		}
+		return -1;
+	}
+
+	private Article getArticleById(int id) {
+//		==v3==
+		int index = getArticleIndexById(id);
+		
+		if (index != -1) {
+			return articles.get(index);
 		}
 		
+//		==v1==
+//		for(int i = 0; i < articles.size(); i++) {
+//			Article article = articles.get(i);
+//			
+//			if(article.id == id) {
+//				return article;
+//			}
+//		}
+		
+//		==v2==
+//		for (Article article : articles) {
+//
+//			if (article.id == id) {
+//				return article;
+//			}
+//		}
+//		
 		return null;
 	}
 
